ab8610d42261e0704b6f07046c4a84192e0361fd
Alan Wu <alanwuu2002@gmail.com>
2024-02-01 02:22:52 +0000

    added role based authorisation

---
+++ changelog.md
@@ -0,0 +1,6 @@
+## [0.01] - 01/02/2024
+### Added
+- Login capability
+- Logout capability
+- Connection with MongoDB
+- Add/Create user capabilty
---
+++ src/middleware.js
@@ -0,0 +1,37 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    console.log(req.nextUrl.pathname);
+    console.log(req.nextauth.token.role);
+
+    if (
+      req.nextUrl.pathname.startsWith("/admin") &&
+      req.nextauth.token.role != "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/Denied", req.url));
+    }
+
+    if (
+      req.nextUrl.pathname.startsWith("/user") &&
+      req.nextauth.token.role != "user"
+    ) {
+      return NextResponse.rewrite(new URL("/Denied", req.url));
+    }
+
+    if (
+      req.nextUrl.pathname.startsWith("/manager") &&
+      req.nextauth.token.role != "manager"
+    ) {
+      return NextResponse.rewrite(new URL("/Denied", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorised: ({ token }) => !!token,
+    },
+  }
+);
+
+export const config = { matcher: ["/admin", "/user", "/manager", "/role-redirect"] };
