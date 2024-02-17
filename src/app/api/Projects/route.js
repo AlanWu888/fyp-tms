08fece34fba0c0e720dea4ccd5c04b175e3aeb95
Alan Wu <alanwuu2002@gmail.com>
2024-02-17 20:19:33 +0000

    WIP Projects API routes

--- src/app/api/Projects/route.js
+++ src/app/api/Projects/route.js
@@ -6,11 +6,25 @@ export async function POST(req) {
     const body = await req.json();
     const projectData = body.formData;
 
+    //Confirm all fields have been filled
+    if (!projectData?.clientname || !projectData?.projectname || !projectData?.deadline || !projectData?.budget || !projectData?.memberEmails) {
+      return NextResponse.json(
+        { message: "All fields are required." },
+        { status: 400 },
+      );
+    }
+
     await Project.create(projectData);
     return NextResponse.json({ message: "Project Created." }, { status: 201 });
   } catch (error) {
     console.log(error);
-    return NextResponse.json({ message: "Error", error }, { status: 500 });
+      if (error.code === 11000) {
+        // Duplicate key error
+        return NextResponse.json({ message: "Duplicate Client and Project combination" }, { status: 409 });
+      } else {
+        // Other errors
+        return NextResponse.json({ message: "An error occurred while saving the project" }, { status: 500 });
+      }
   }
 }
 
@@ -20,6 +34,36 @@ export async function GET() {
     console.log(projects);
     return NextResponse.json({ projects }, { status: 201 });
   } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function PATCH(req) {
+  try {
+    const body=await req.json()
+    const {clientname, projectname, newData} = body
+
+    // Check if ProjectId is provided
+    if (!clientname || !projectname) {
+      return NextResponse.json(
+        { message: "Project ID is required." },
+        { status: 400 },
+      );
+    }
+    const updatedProject = await Project.findOneAndUpdate({clientname, projectname}, newData, {
+      new: true,
+    });
+
+    if (!updatedProject) {
+      return NextResponse.json({ message: "Project not found." }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "Project updated.", project: updatedProject },
+      { status: 200 },
+    );
+  } catch(error){
     console.log(error);
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
