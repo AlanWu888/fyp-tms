c2ee817ce6950b22ab59ef9c1650c255b5807353
Alan Wu <alanwuu2002@gmail.com>
2024-02-17 16:39:10 +0000

    added delete user API and added password hashing when reseting passwords in patch method

--- src/app/api/Users/route.js
+++ src/app/api/Users/route.js
@@ -56,17 +56,60 @@ export async function PATCH(req) {
 
     // Check if userId is provided
     if (!userId) {
-      return NextResponse.json({ message: "User ID is required." }, { status: 400 });
+      return NextResponse.json(
+        { message: "User ID is required." },
+        { status: 400 },
+      );
+    }
+
+    // If there's a new password provided, hash it
+    if (newData.password) {
+      newData.password = await bcrypt.hash(newData.password, 10);
     }
 
     // Update the user
-    const updatedUser = await User.findByIdAndUpdate(userId, newData, { new: true });
+    const updatedUser = await User.findByIdAndUpdate(userId, newData, {
+      new: true,
+    });
 
     if (!updatedUser) {
       return NextResponse.json({ message: "User not found." }, { status: 404 });
     }
 
-    return NextResponse.json({ message: "User updated.", user: updatedUser }, { status: 200 });
+    return NextResponse.json(
+      { message: "User updated.", user: updatedUser },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    const body = await req.json();
+    const { userId } = body; // Assuming you'll send userId in the request body
+
+    // Check if userId is provided
+    if (!userId) {
+      return NextResponse.json(
+        { message: "User ID is required." },
+        { status: 400 },
+      );
+    }
+
+    // Find and delete the user
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return NextResponse.json({ message: "User not found." }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "User deleted.", user: deletedUser },
+      { status: 200 },
+    );
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: "Error", error }, { status: 500 });
