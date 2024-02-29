5f4e0ebd82943552d67eed1665f1a6e6dca54dc6
Alan Wu <alanwuu2002@gmail.com>
2024-02-29 19:58:56 +0000

    json displaying on project screen for managers

--- src/app/manager/project/page.jsx
+++ src/app/manager/project/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavManager from "@/app/(components)/navigation/NavManager";
+import ProjectsComponent from "@/app/(components)/projects/manager/json";
 
 const ManagerProjects = () => {
   return (
     <div>
       <NavManager />
       <div>ManagerProjects</div>
+      <ProjectsComponent/>
     </div>
   );
 };
