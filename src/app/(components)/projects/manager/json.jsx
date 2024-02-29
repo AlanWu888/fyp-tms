ce71967ed40823c9c87e5dffff6a0338b3b3c038
Alan Wu <alanwuu2002@gmail.com>
2024-02-29 20:01:11 +0000

    formatting

--- src/app/(components)/projects/manager/json.jsx
+++ src/app/(components)/projects/manager/json.jsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function ProjectsComponent() {
   const [projects, setProjects] = useState([]);
@@ -10,15 +10,15 @@ function ProjectsComponent() {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('/api/Projects', {
-          method: 'GET',
+        const response = await fetch("/api/Projects", {
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json'
-          }
+            "Content-Type": "application/json",
+          },
         });
 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
 
         const data = await response.json();
@@ -42,17 +42,17 @@ function ProjectsComponent() {
   }
 
   return (
-    <div >
+    <div>
       <h1>Projects</h1>
       <ul>
-        {projects.map(project => (
+        {projects.map((project) => (
           <div className="border border-black p-3 m-3">
             <li key={project._id}>
               <p>Client Name: {project.clientname}</p>
               <p>Project Name: {project.projectname}</p>
               <p>Deadline: {project.deadline}</p>
               <p>Budget: {project.budget}</p>
-              <p>Member Emails: {project.memberEmails.join(', ')}</p>
+              <p>Member Emails: {project.memberEmails.join(", ")}</p>
             </li>
           </div>
         ))}
