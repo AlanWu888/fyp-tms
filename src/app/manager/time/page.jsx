9fbc00b9e872279cdbbb13c6a8b35cd3e6b8e4a2
Alan Wu <alanwuu2002@gmail.com>
2024-02-29 14:39:09 +0000

    json displaying on user and manager screens

--- src/app/manager/time/page.jsx
+++ src/app/manager/time/page.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import NavManager from "@/app/(components)/navigation/NavManager";
 import TestComponent from "@/app/(components)/timesheet/test";
+import TimesheetComponent from "@/app/(components)/timesheet/json";
 
 const ManagerTime = () => {
   return (
     <div>
       <NavManager />
       <div>ManagerTime</div>
-      <TestComponent />
+      {/* <TestComponent /> */}
+      <TimesheetComponent />
     </div>
   );
 };
--- src/app/user/time/page.jsx
+++ src/app/user/time/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavUser from "@/app/(components)/navigation/NavUser";
+import TimesheetComponent from "@/app/(components)/timesheet/json";
 
 const UserTime = () => {
   return (
     <div>
       <NavUser />
       <div>UserTime</div>
+      <TimesheetComponent />
     </div>
   );
 };
