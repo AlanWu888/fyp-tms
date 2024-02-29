bc3f82599d845400800549726166a397ef3fec68
Alan Wu <alanwuu2002@gmail.com>
2024-02-29 19:59:48 +0000

    navigation tabs component

--- src/app/Denied/Components/page.jsx
+++ src/app/Denied/Components/page.jsx
@@ -1,8 +1,14 @@
 import MyComponents from "@/app/(components)/all-compoents";
+import NavTabs from "@/app/(components)/navigation/NavTabs";
 import React from "react";
 
 const Components = () => {
-  return <MyComponents />;
+  return (
+    <div>
+    <MyComponents />
+    <NavTabs items={['Home', 'About', 'Services', 'Contact']}/>
+    </div>
+  );
 };
 
 export default Components;
