71887631ce985241be311e1be928007e66f87b97
Alan Wu <alanwuu2002@gmail.com>
2024-03-12 16:50:14 +0000

    able to add users but does not refresh the team list, and redirect broken

--- src/app/api/Projects/route.js
+++ src/app/api/Projects/route.js
@@ -66,41 +66,23 @@ export async function PATCH(req) {
 
     let updatedProject;
 
-    // If newData contains memberEmails, handle them
-    if (newData && newData.memberEmails) {
-      // Find the existing project
-      const existingProject = await Project.findOne({
-        clientname,
-        projectname,
-      });
+    const existingProject = await Project.findOne({
+      clientname,
+      projectname,
+    });
 
-      if (!existingProject) {
-        return NextResponse.json(
-          { message: "Project not found." },
-          { status: 404 },
-        );
-      }
-
-      // Remove duplicates from newData.memberEmails and append them
-      const uniqueMemberEmails = [...new Set(newData.memberEmails)];
-
-      // Update newData with unique memberEmails
-      newData.memberEmails = uniqueMemberEmails;
-
-      // Merge newData with existing project data
-      updatedProject = await Project.findOneAndUpdate(
-        { clientname, projectname },
-        { $addToSet: newData },
-        { new: true },
-      );
-    } else {
-      // If newData does not contain memberEmails, update the project directly
-      updatedProject = await Project.findOneAndUpdate(
-        { clientname, projectname },
-        newData,
-        { new: true },
+    if (!existingProject) {
+      return NextResponse.json(
+        { message: "Project not found." },
+        { status: 404 },
       );
     }
+
+    updatedProject = await Project.findOneAndUpdate(
+      { clientname, projectname },
+      newData,
+      { new: true },
+    );
 
     // Check if project exists and return the updated project
     if (!updatedProject) {
