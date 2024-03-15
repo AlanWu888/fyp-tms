8ba74c669cc371604932c897bbc5f5bbb2d05928
Alan Wu <alanwuu2002@gmail.com>
2024-03-15 17:47:15 +0000

    edit user form and page created

---
+++ src/app/admin/edit-user/page.jsx
@@ -0,0 +1,42 @@
+import EditUser from "@/app/(components)/admin/EditUser/EditUser";
+import GoBack from "@/app/(components)/buttons/GoBack";
+import NavAdmin from "@/app/(components)/navigation/NavAdmin";
+import Link from "next/link";
+import React from "react";
+
+const CreateUser = () => {
+  return (
+    <div>
+      <NavAdmin />
+      <div
+        style={{
+          paddingLeft: "10%",
+          paddingRight: "10%",
+          paddingTop: "30px",
+        }}
+      >
+        <Link
+          href={{
+            pathname: `/admin`,
+          }}
+        >
+          <GoBack />
+        </Link>
+        <div
+          className="edit-user-header"
+          style={{
+            paddingBottom: "10px",
+            borderBottom: "1px solid black",
+            marginBottom: "20px",
+          }}
+        >
+          <div style={{ fontSize: "22px", fontWeight: "bold" }}>Edit User</div>
+          <p>Edit user information here</p>
+        </div>
+        <EditUser />
+      </div>
+    </div>
+  );
+};
+
+export default CreateUser;
