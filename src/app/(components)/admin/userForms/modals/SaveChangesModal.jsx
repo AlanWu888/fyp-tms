a4fb99f80d36d74f0d16a1f508cc8d0a24c7a02d
Alan Wu <alanwuu2002@gmail.com>
2024-03-26 17:12:27 +0000

    Edit user and password update modals added

---
+++ src/app/(components)/admin/userForms/modals/SaveChangesModal.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { COLOURS } from "@/app/constants";
+
+function SaveChangesModal({ onClose, user }) {
+  return (
+    <div className="modal" style={modalStyle}>
+      <div className="modal-content" style={modalContentStyle}>
+        <div
+          className="modal-header"
+          style={{
+            backgroundColor: COLOURS.GREY,
+            margin: "auto",
+            padding: "10px",
+            justifyContent: "center",
+            display: "flex",
+            fontWeight: "bold",
+            borderTopLeftRadius: "10px",
+            borderTopRightRadius: "10px",
+            borderBottom: "1px solid black",
+          }}
+        >
+          <h2>User Changes Saved</h2>
+        </div>
+
+        <div
+          style={{
+            paddingLeft: "20px",
+            paddingRight: "20px",
+            paddingBottom: "20px",
+          }}
+        >
+          <p>Updated user data:</p>
+          <p>Email: {user.email}</p>
+          <p>First name: {user.firstname}</p>
+          <p>Last name: {user.lastname}</p>
+          <p>Capacity: {user.capacity}</p>
+          <p>Role: {user.role}</p>
+
+          <div
+            className="modal-button-group"
+            style={{
+              display: "flex",
+              marginTop: "35px",
+              justifyContent: "flex-end",
+            }}
+          >
+            <button
+              onClick={onClose}
+              style={{
+                padding: "5px 10px",
+                backgroundColor: COLOURS.GREY,
+                border: "1px solid black",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SaveChangesModal;
+
+const modalStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "fixed",
+  zIndex: "1",
+  left: "0",
+  top: "0",
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0,0,0,0.4)",
+};
+
+const modalContentStyle = {
+  width: "500px",
+  backgroundColor: "#fff",
+  borderRadius: "10px",
+};
